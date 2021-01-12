f999ce8ca88657bfff2afedbbbb29ba1f3d79443
Jeehyun Yang <jeehyun@mit.edu>
2021-01-12 11:38:18 -0500

    run black on functions

--- molecool/functions.py
+++ molecool/functions.py
@@ -5,35 +5,6 @@ A python package for analyzing and visualizing molecular files for Molssi Worksh
 Handles the primary functions
 """
 
-
-def canvas(with_attribution=True):
-    """
-    Placeholder function to show example docstring (NumPy format)
-
-    Replace this function and doc string for your own project
-
-    Parameters
-    ----------
-    with_attribution : bool, Optional, default: True
-        Set whether or not to display who the quote is from
-
-    Returns
-    -------
-    quote : str
-        Compiled string including quote and optional attribution
-    """
-
-    quote = "The code is but a canvas to our imagination."
-    if with_attribution:
-        quote += "\n\t- Adapted from Henry David Thoreau"
-    return quote
-
-
-if __name__ == "__main__":
-    # Do something if this file is invoked on its own
-    print(canvas())
-
-
 import os
 import numpy as np
 import matplotlib.pyplot as plt
@@ -211,3 +182,31 @@ atom_colors = {
     "Br": "#F4A460",
     "S": "yellow",
 }
+
+
+def canvas(with_attribution=True):
+    """
+    Placeholder function to show example docstring (NumPy format)
+
+    Replace this function and doc string for your own project
+
+    Parameters
+    ----------
+    with_attribution : bool, Optional, default: True
+        Set whether or not to display who the quote is from
+
+    Returns
+    -------
+    quote : str
+        Compiled string including quote and optional attribution
+    """
+
+    quote = "The code is but a canvas to our imagination."
+    if with_attribution:
+        quote += "\n\t- Adapted from Henry David Thoreau"
+    return quote
+
+
+if __name__ == "__main__":
+    # Do something if this file is invoked on its own
+    print(canvas())
